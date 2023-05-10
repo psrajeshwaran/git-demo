a8c611f09791d11f5c035d6d9b89b278b408377e
Rajeshwaran <psrajeshwaran.com>
2023-05-10 20:23:27 +0530

    added to local repo

---
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -0,0 +1,17 @@
+package com.javatechie.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String Data) {
+		System.out.println("DATA="+ Data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
