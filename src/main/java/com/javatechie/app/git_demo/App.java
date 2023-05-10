b6bff6667de81f926038adeb1caf40d8c6155017
Rajeshwaran <psrajeshwaran.com>
2023-05-10 20:31:22 +0530

    added null condition check

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -7,8 +7,11 @@ package com.javatechie.app.git_demo;
 public class App 
 {
 	
-	public void addData(String Data) {
-		System.out.println("DATA="+ Data);
+	public void addData(String data) {
+		System.out.println("DATA="+ data);
+		if(data ==null) {
+			System.out.println("data is null");
+		}
 	}
     public static void main( String[] args )
     {
